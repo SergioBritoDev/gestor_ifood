bc77a6c01179245bffaf04f4b3df6b89c1dc9067
imac <imac@iMac-do-Sergio.local>
2025-05-03 21:24:02 -0300

    fix: allow unsafe werkzeug for socketio in Render

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def finalizar_pedido(data):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), allow_unsafe_werkzeug=True)
 
